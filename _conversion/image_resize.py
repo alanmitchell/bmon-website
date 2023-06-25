c4663d8ecbb210d2f15d42bfdac82fe4b45e13aa
Alan Mitchell <tabb99@gmail.com>
2023-06-25 14:40:29 -0800

    Inital image resizing.

--- _conversion/image_resize.py
+++ _conversion/image_resize.py
@@ -5,17 +5,10 @@ import re
 from PIL import Image
 import yaml
 
+FULL_WIDTH = 936      # Image width in pixels for full-width image.
+
 # the base directory for the images
 base_dir = sys.argv[1]
-
-def resize_images(image_directory, new_width, new_height):
-    for filename in os.listdir(image_directory):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            img = Image.open(os.path.join(image_directory, filename))
-            img = img.resize((new_width, new_height), Image.ANTIALIAS)
-            img.save(os.path.join(image_directory, filename))
-
-#resize_images('/path/to/your/images', 800, 600)
 
 # open the YAML file containing resizing info for this set of images
 img_info = yaml.safe_load(open(f'image-{base_dir}.yaml', 'r'))
@@ -26,7 +19,7 @@ print(img_info)
 source_dir = f'converted/{base_dir}/media'
 out_dir = f'../assets/guide/{base_dir}'
 for filename in os.listdir(source_dir):
-    if filename.endswith('.jpg') or filename.endswith('.png'):
+    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
         print(filename)
         # get image number
         match = re.search(r"image(\d+)", filename)
@@ -37,11 +30,12 @@ for filename in os.listdir(source_dir):
         aspect_ratio = height/width
 
         if img_num in img_info:
-            new_width = int(790 * img_info[img_num])
+            new_width = int(FULL_WIDTH * img_info[img_num])
             print(img_num, new_width)
         else:
-            new_width = 790
+            new_width = FULL_WIDTH
         new_height = int(new_width * aspect_ratio)
 
         img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
-        img.save(os.path.join(out_dir, filename))
+        img.save(os.path.join(out_dir, filename), quality=95)
+        img.close()
