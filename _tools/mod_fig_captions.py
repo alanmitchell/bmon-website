6ea1c32b7a1e44cd358a3aed5556b25254a8a25e
Alan Mitchell <tabb99@gmail.com>
2023-06-28 08:42:28 -0800

    Improved script to modify figure captions.
    Decided not to use figcaption tags due to complications
    with embedded HTML and margin formatting.

---
+++ _tools/mod_fig_captions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Script to replace italicized figure captions with <figcaption> HTML
+tagged captions.
+"""
+import re
+from pathlib import Path
+
+md_files = [
+#    'intro',
+    'sensors',
+#    'setup-bmon',
+#    'use-data',
+    ]
+
+base_dir = Path('../guide')
+
+for f in md_files:
+    print(f)
+
+    with open(f'out/{f}.md', 'w') as fout:
+
+        fin = open(base_dir / f'{f}.md')
+        while True:
+
+            lin = fin.readline()
+
+            if lin == '':
+                break
+
+            lin = lin.rstrip()
+
+            if '<br>{: #image' in lin:
+                if '*' in lin:
+                    # if this line doesn't have the full caption, read subsequent lines and add them
+                    while not lin.endswith('*'):
+                        lin += ' ' + fin.readline().strip()
+
+                    mod_lin = lin[:-1].replace('*', '\n<figcaption markdown="1">\n') + '\n</figcaption>'
+                    print(mod_lin, file=fout)
+                    
+                else:
+                    print(lin, file=fout)
+
+            else:
+                print(lin, file=fout)
